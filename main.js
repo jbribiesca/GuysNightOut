4dfd9dfc8bc537a9d2b4bf2e11f9e9b704a8027c
Jeremy <jeremy.cai@ymail.com>
2019-03-27 20:04:53 -0500

    css improved and events location added

--- main.js
+++ main.js
@@ -17,8 +17,9 @@ function eventGet() {
         method: "GET"
     }).then(function (response) {
         urlPool = [];
-        captionPool = [];
+        performerPool = [];
         linkPool = [];
+        venuePool = [];
         for (var i=0; i<9; i++) {
             var imgURL = response.events[i].performers[0].image;
             if(imgURL === null){
@@ -28,9 +29,11 @@ function eventGet() {
             console.log(imgURL);
             var caption = response.events[i].title;
             var eventLink = response.events[i].url;
+            var location = response.events[i].venue.name_v2;
             urlPool.push(imgURL);
-            captionPool.push(caption);
+            performerPool.push(caption);
             linkPool.push(eventLink);
+            venuePool.push(location);
         }
         console.log("display");
         display();
@@ -46,7 +49,12 @@ function display() {
         var newImg = $("<img>");
         newImg.attr("src", urlPool[j]);
         newImg.attr("class", "event-thumb figure-img img-fluid rounded");
-        var newCap = $("<figcaption>").text(captionPool[j]);
+        var venue = $("<p>").text(venuePool[j]);
+        venue.attr("class", "venue");
+        var performer = $("<p>").text(performerPool[j]);
+        performer.attr("class", "performer");
+        var newCap = $("<figcaption>");
+        newCap.append(performer, venue);
         newFigure.append(newImg, newCap);
         var newLink = $("<a>").html(newFigure);
         newLink.attr("href", linkPool[j]);
